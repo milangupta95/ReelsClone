17229c7d2ce5319de253cad3f74c8867251e7b0a
milangupta95 <milangupta95@gmail.com>
2023-05-10 02:45:46 +0530

    Cors Removed

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     credentials: true,
+    origin: 'http://localhost:3000'
 }));
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
