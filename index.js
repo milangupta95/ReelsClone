0762366a1c287a28478a2f94b8b0090e37f258cd
milangupta95 <milangupta95@gmail.com>
2023-05-12 03:01:43 +0530

    Removed Bugs

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ app.get('/video/:filename', (req, res) => {
     const videoStream = fs.createReadStream(videoPath, { start, end });
     videoStream.pipe(res);
 });
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (_, res) {
     var filePath = "./client/build/index.html";
     var resolvedPath = path.resolve(filePath);
